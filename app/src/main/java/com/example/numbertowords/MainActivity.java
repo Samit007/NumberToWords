cf2830e7a3fbc0f669cff919be571e7e2684f911
Samit Bhattarai <bhattarai905@gmail.com>
2019-04-09 16:56:53 +0545

    Initial commit

---
+++ app/src/main/java/com/example/numbertowords/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.numbertowords;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etnumber;
+    private TextView result;
+    private Button btnconvert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etnumber = findViewById(R.id.etnumber);
+        result = findViewById(R.id.result);
+        btnconvert = findViewById(R.id.btnconvert);
+
+        btnconvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
